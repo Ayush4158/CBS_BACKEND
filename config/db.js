bb51ba63b739681482d7382ff6062dfe0aa86abc
Ayush4158 <ayushsingh8692@gmail.com>
2026-06-15 10:32:54 +0530

    imported the dotenv to get the env access

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
-import {Pool} from "pg"
+// config/db.js
+import pg from 'pg';
+const { Pool } = pg;
+import 'dotenv/config';
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+const pool = new Pool({
+  // Append sslmode=require directly to the connection string
+  connectionString: process.env.DATABASE_URL
 });
+
+export { pool };
